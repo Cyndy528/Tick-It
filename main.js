0a40f3acd1327b6fde73ffa26dc9ef3341338a28
Cynthia Salinas <csalinas123@gmail.com>
2015-11-30 21:43:08 -0800

    set up handle bars client side

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
 $(function(){
 	
+	// compile handlebars template 
+	var source = $ ('#tickets-template').html(); 
+	var template = Handlebars.compile(source); 
+
+
+	// array of test data
+	var allTickets = [
+		{ ticket: 'Need help', message: 'Computer equipment is broken'}, 
+		{ ticket: 'Visitors Coming', message: 'Offices needed for guests'}
+	]; 
+
+	var ticketsHtml = template ({
+		tickets: allTickets
+	});
+	$('#tickets-list').append(ticketsHtml); 
 }); 
