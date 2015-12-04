69259e913b1ec59cd46c1b862887df2675b62060
Cynthia Salinas <csalinas123@gmail.com>
2015-12-03 16:34:35 -0800

    comment routes out for first deploy

--- server.js
+++ server.js
@@ -74,148 +74,148 @@ app.get('/profile', function (req, res){
 
 // sign up new user, then log them in
 // hashes and salts password, saves new user to db
-app.post('/', function (req, res) {
-  // if user is logged in, don't let them sign up again
-  if (req.user) {
-    res.redirect('/profile');
-  } else {
-    User.register(new User({ username: req.body.username }), req.body.password,
-      function (err, newUser) {
-        passport.authenticate('local')(req, res, function () {
-          res.redirect('/profile');
-        });
-      }
-    );
-  }
-});
+// app.post('/profile', function (req, res) {
+//   // if user is logged in, don't let them sign up again
+//   if (req.user) {
+//     res.redirect('/profile');
+//   } else {
+//     User.register(new User({ username: req.body.username }), req.body.password,
+//       function (err, newUser) {
+//         passport.authenticate('local')(req, res, function () {
+//           res.redirect('/profile');
+//         });
+//       }
+//     );
+//   }
+// });
 
 // show profile view
-app.get('/profile', function (req, res) {
-  if (req.user) {
-    res.redirect('/profile');
-  } else {
-    res.render('profile', { user: req.user });
-  }
-});
+// app.get('/profile', function (req, res) {
+//   if (req.user) {
+//     res.redirect('/profile');
+//   } else {
+//     res.render('profile', { user: req.user });
+//   }
+// });
 
 // log in user
-app.post('/profile', passport.authenticate('local'), function (req, res) {
-  res.redirect('/profile');
-});
+// app.post('/profile', passport.authenticate('local'), function (req, res) {
+//   res.redirect('/profile');
+// });
 
 // log out user
-app.get('/logout', function (req, res) {
-  req.logout();
-  res.redirect('/');
-});
+// app.get('/logout', function (req, res) {
+//   req.logout();
+//   res.redirect('/');
+// });
 
 // show user profile page
-app.get('/profile', function (req, res) {
-  // only show profile if user is logged in
-  if (req.user) {
-    res.render('profile', { user: req.user });
-  } else {
-    res.redirect('/index');
-  }
-});
+// app.get('/profile', function (req, res) {
+//   // only show profile if user is logged in
+//   if (req.user) {
+//     res.render('profile', { user: req.user });
+//   } else {
+//     res.redirect('/index');
+//   }
+// });
 
 
 // API ROUTES
 
 // get all tickets
-app.get('/api/tickets', function (req, res) {
-  // find all tickets in db
-  Ticket.find(function (err, allTickets) {
-    if (err) {
-      res.status(500).json({ error: err.message });
-    } else {
-      res.json({ tickets: allTickets });
-    }
-  });
-});
+// app.get('/api/tickets', function (req, res) {
+//   // find all tickets in db
+//   Ticket.find(function (err, allTickets) {
+//     if (err) {
+//       res.status(500).json({ error: err.message });
+//     } else {
+//       res.json({ tickets: allTickets });
+//     }
+//   });
+// });
 
-// create new ticket
-app.post('/api/tickets', function (req, res) {
-  if (req.user) {
-    // create new ticket with form data (`req.body`)
-    var newTicket = new Ticket(req.body);
+// // create new ticket
+// app.post('/api/tickets', function (req, res) {
+//   if (req.user) {
+//     // create new ticket with form data (`req.body`)
+//     var newTicket = new Ticket(req.body);
 
-    // save new ticket in db
-    newTicket.save(function (err, savedTicket) {
-      if (err) {
-        res.status(500).json({ error: err.message });
-      } else {
-        req.user.tickets.push(savedTicket);
-        req.user.save();
-        res.json(savedTicket);
-      }
-    });
-  } else {
-    res.status(401).json({ error: 'Unauthorized.' });
-  }
-});
+//     // save new ticket in db
+//     newTicket.save(function (err, savedTicket) {
+//       if (err) {
+//         res.status(500).json({ error: err.message });
+//       } else {
+//         req.user.tickets.push(savedTicket);
+//         req.user.save();
+//         res.json(savedTicket);
+//       }
+//     });
+//   } else {
+//     res.status(401).json({ error: 'Unauthorized.' });
+//   }
+// });
 
-// get one ticket
-app.get('/api/tickets/:id', function (req, res) {
-  // get ticket id from url params (`req.params`)
-  var ticketId = req.params.id;
+// // get one ticket
+// app.get('/api/tickets/:id', function (req, res) {
+//   // get ticket id from url params (`req.params`)
+//   var ticketId = req.params.id;
 
   // find ticket in db by id
-  Ticket.findOne({ _id: ticketId }, function (err, foundTicket) {
-    if (err) {
-      if (err.name === "CastError") {
-        res.status(404).json({ error: "Nothing found by this ID." });
-      } else {
-        res.status(500).json({ error: err.message });
-      }
-    } else {
-      res.json(foundTicket);
-    }
-  });
-});
+//   Ticket.findOne({ _id: ticketId }, function (err, foundTicket) {
+//     if (err) {
+//       if (err.name === "CastError") {
+//         res.status(404).json({ error: "Nothing found by this ID." });
+//       } else {
+//         res.status(500).json({ error: err.message });
+//       }
+//     } else {
+//       res.json(foundTicket);
+//     }
+//   });
+// });
 
 // update ticket
-app.put('/api/tickets/:id', function (req, res) {
-  // get ticket id from url params (`req.params`)
-  var ticketId = req.params.id;
+// app.put('/api/tickets/:id', function (req, res) {
+//   // get ticket id from url params (`req.params`)
+//   var ticketId = req.params.id;
 
   // find ticket in db by id
-  Ticket.findOne({ _id: ticketId }, function (err, foundTicket) {
-    if (err) {
-      res.status(500).json({ error: err.message });
-    } else {
-      // update the ticket's attributes
-      foundTicket.completed = req.body.completed;
-      foundTicket.department = req.body.department;
-      foundTicket.description = req.body.description;
-      foundTicket.createdAt = req.body.createdAt; 
+//   Ticket.findOne({ _id: ticketId }, function (err, foundTicket) {
+//     if (err) {
+//       res.status(500).json({ error: err.message });
+//     } else {
+//       // update the ticket's attributes
+//       foundTicket.completed = req.body.completed;
+//       foundTicket.department = req.body.department;
+//       foundTicket.description = req.body.description;
+//       foundTicket.createdAt = req.body.createdAt; 
 
-      // save updated ticket in db
-      foundTicket.save(function (err, savedTicket) {
-        if (err) {
-          res.status(500).json({ error: err.message });
-        } else {
-          res.json(savedTicket);
-        }
-      });
-    }
-  });
-});
+//       // save updated ticket in db
+//       foundTicket.save(function (err, savedTicket) {
+//         if (err) {
+//           res.status(500).json({ error: err.message });
+//         } else {
+//           res.json(savedTicket);
+//         }
+//       });
+//     }
+//   });
+// });
 
 // delete ticket
-app.delete('/api/tickets/:id', function (req, res) {
-  // get ticket id from url params (`req.params`)
-  var ticketId = req.params.id;
+// app.delete('/api/tickets/:id', function (req, res) {
+//   // get ticket id from url params (`req.params`)
+//   var ticketId = req.params.id;
 
-  // find ticket in db by id and remove
-  Ticket.findOneAndRemove({ _id: ticketId }, function (err, deletedDeleted) {
-    if (err) {
-      res.status(500).json({ error: err.message });
-    } else {
-      res.json(deletedTicket);
-    }
-  });
-});
+//   // find ticket in db by id and remove
+//   Ticket.findOneAndRemove({ _id: ticketId }, function (err, deletedDeleted) {
+//     if (err) {
+//       res.status(500).json({ error: err.message });
+//     } else {
+//       res.json(deletedTicket);
+//     }
+//   });
+// });
 
 
 // listen on port 3000
